74ed6834b5b6ff5ff99dee61f9592512c2e48dd4
dominiciv <leonel.saldivia2017@gmail.com>
2025-02-25 21:14:40 -0300

    Update BackgroundImage.tsx

--- app/components/BackgroundImage.tsx
+++ app/components/BackgroundImage.tsx
@@ -1,16 +1,17 @@
+
 export default function BackgroundImage() {
   return (
-    <div
-      className="absolute inset-0 z-0"
-      style={{
-        backgroundImage:
-          "url('https://hebbkx1anhila5yf.public.blob.vercel-storage.com/pagina_1920x1080-T3TOfBfZZFcRdXkauNWodQ28Yxag5b.png')",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundColor: "black", // Fallback color
-      }}
-    />
-  )
+    <div className="absolute inset-0 z-0">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="w-full h-full object-cover"
+      >
+        <source src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/video.mp4" type="video/mp4" />
+        Tu navegador no soporta videos.
+      </video>
+    </div>
+  );
 }
-
