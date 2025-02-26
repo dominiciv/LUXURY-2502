b8d2ce775eb04bf89b6a2b0b7aec3d812a88d05f
dominiciv <leonel.saldivia2017@gmail.com>
2025-02-25 21:52:31 -0300

    Update BackgroundImage.tsx

--- app/components/BackgroundImage.tsx
+++ app/components/BackgroundImage.tsx
@@ -1,13 +1,15 @@
 export default function BackgroundImage() {
   return (
-    <div className="absolute inset-0 z-0 w-full h-full overflow-hidden">
-      <iframe
-        className="absolute top-0 left-0 w-full h-full"
-        src="https://www.youtube.com/embed/qcJFkx5NDWM?autoplay=1&mute=1&loop=1&playlist=YOUTUBE_VIDEO_ID&controls=0&showinfo=0&modestbranding=1"
-        title="Video de fondo"
-        frameBorder="0"
-        allow="autoplay; fullscreen"
-      />
-    </div>
-  );
+    <div
+      className="absolute inset-0 z-0"
+      style={{
+        backgroundImage:
+          "url('https://hebbkx1anhila5yf.public.blob.vercel-storage.com/pagina_1920x1080-T3TOfBfZZFcRdXkauNWodQ28Yxag5b.png')",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundColor: "black", // Fallback color
+      }}
+    />
+  )
 }
